3af7cf9eff76991972502dc181304667d065ff63
harris2001 <harrishadjiantonis@gmail.com>
2026-03-19 23:53:42 +0000

    Making log dependency globally accessible

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,6 @@
 #define VERSION "0.1"
 
 int main() {
-	std::cout << "Ultra Low Latency Feed Handler v" << VERSION << std::endl;
+	std::cout << "Ultra Low Latency Feed Handler v" << VERSION << '\n';
 	return 0;
 }
