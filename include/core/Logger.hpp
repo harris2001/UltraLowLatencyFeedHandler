3deedd395de02c8154ba5f7ecf65f7d1f202231b
harris2001 <harrishadjiantonis@gmail.com>
2026-03-22 21:27:13 +0000

    Improve logging diagnostic mode

--- include/core/Logger.hpp
+++ include/core/Logger.hpp
@@ -33,6 +33,7 @@ class Logger {
             
             spdlog::register_logger(logger);
             spdlog::set_default_logger(logger);
+            logger->info("Logger initialized with level: {}", spdlog::level::to_string_view(level));
         } catch (const spdlog::spdlog_ex& ex) {
             // Silent: (I don't want logging infrastructure to crash the feed handler)
         }
@@ -65,33 +66,38 @@ class Logger {
 
 }  // namespace ullfh::core
 
-#define ULLFH_LOG_TRACE(...) \
-    ::ullfh::core::Logger::get()->trace(__VA_ARGS__)
+// ---- Logging macros ----
+// In Release builds we compile out TRACE/DEBUG/INFO/WARN entirely to remove overhead from the hot path.
+// But we retain ERROR and CRITICAL to catch faults.
 
-#define ULLFH_LOG_DEBUG(...) \
-    ::ullfh::core::Logger::get()->debug(__VA_ARGS__)
+#ifdef ULLFH_DIAGNOSTICS
+#define ULLFH_LOG_TRACE(...)    ::ullfh::core::Logger::get()->trace(__VA_ARGS__)
+#define ULLFH_LOG_DEBUG(...)    ::ullfh::core::Logger::get()->debug(__VA_ARGS__)
+#define ULLFH_LOG_INFO(...)     ::ullfh::core::Logger::get()->info(__VA_ARGS__)
+#define ULLFH_LOG_WARN(...)     ::ullfh::core::Logger::get()->warn(__VA_ARGS__)
+#else
+#define ULLFH_LOG_TRACE(...)    do {} while(0)
+#define ULLFH_LOG_DEBUG(...)    do {} while(0)
+#define ULLFH_LOG_INFO(...)     do {} while(0)
+#define ULLFH_LOG_WARN(...)     do {} while(0)
+#endif
 
-#define ULLFH_LOG_INFO(...) \
-    ::ullfh::core::Logger::get()->info(__VA_ARGS__)
+#define ULLFH_LOG_ERROR(...)    ::ullfh::core::Logger::get()->error(__VA_ARGS__)
+#define ULLFH_LOG_CRITICAL(...) ::ullfh::core::Logger::get()->critical(__VA_ARGS__)
 
-#define ULLFH_LOG_WARN(...) \
-    ::ullfh::core::Logger::get()->warn(__VA_ARGS__)
-
-#define ULLFH_LOG_ERROR(...) \
-    ::ullfh::core::Logger::get()->error(__VA_ARGS__)
-
-#define ULLFH_LOG_CRITICAL(...) \
-    ::ullfh::core::Logger::get()->critical(__VA_ARGS__)
-
-// Error-specific macros
+// Error-specific macros - only used for diagnostics
+#ifdef ULLFH_DIAGNOSTICS
 #define ULLFH_LOG_PARSE_ERROR(code, msg) \
     ::ullfh::core::Logger::get()->warn("Parse error: {} - {}", \
         ::ullfh::core::error_string(code), msg)
-
 #define ULLFH_LOG_QUEUE_ERROR(code) \
     ::ullfh::core::Logger::get()->warn("Queue error: {}", \
         ::ullfh::core::error_string(code))
-
 #define ULLFH_LOG_BUFFER_ERROR(code) \
     ::ullfh::core::Logger::get()->warn("Buffer error: {}", \
         ::ullfh::core::error_string(code))
+#else
+#define ULLFH_LOG_PARSE_ERROR(code, msg)  do {} while(0)
+#define ULLFH_LOG_QUEUE_ERROR(code)       do {} while(0)
+#define ULLFH_LOG_BUFFER_ERROR(code)      do {} while(0)
+#endif
