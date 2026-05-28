6d3aeecd4ddcf94605d69345582ab21d36f9283a
harris2001 <harrishadjiantonis@gmail.com>
2026-05-28 23:10:20 +0100

    Formating main

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,16 @@
-#include <iostream>
 #include <string>
 #include <unordered_map>
+
 #include "core/Logger.hpp"
 
 #define VERSION "0.1"
 
 spdlog::level::level_enum parse_log_level(const std::string& level_str) {
     static const std::unordered_map<std::string, spdlog::level::level_enum> level_map = {
-        {"trace", spdlog::level::trace},
-        {"debug", spdlog::level::debug},
-        {"info", spdlog::level::info},
-        {"warn", spdlog::level::warn},
-        {"err", spdlog::level::err},
-        {"critical", spdlog::level::critical},
-        {"off", spdlog::level::off}
-    };
-    
+         {"trace", spdlog::level::trace}, {"debug", spdlog::level::debug}, {"info", spdlog::level::info},
+         {"warn", spdlog::level::warn},   {"err", spdlog::level::err},     {"critical", spdlog::level::critical},
+         {"off", spdlog::level::off}};
+
     auto it = level_map.find(level_str);
     return (it != level_map.end()) ? it->second : spdlog::level::info;
 }
@@ -25,10 +20,10 @@ int main(int argc, char* argv[]) {
     if (argc > 1) {
         log_level = argv[1];
     }
-    
-	using namespace ullfh::core;
+
+    using namespace ullfh::core;
     Logger::init("ullfh", parse_log_level(log_level));
-    
+
     auto logger = Logger::get();
     logger->info("Logger initialized successfully.");
     Logger::shutdown();
